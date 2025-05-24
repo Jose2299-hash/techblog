f687c44a6fab8781425a48366d432d685ed0804d
Jose2299-hash <degraciaj2299@gmail.com>
2025-05-23 21:54:05 -0500

    Partes Adicionales

--- about/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from about.views import showAboutMe, showAboutProject
-
-urlpatterns = [
-    path('/me', showAboutMe, name='about_me'),
-    path('/project', showAboutProject, name='about_project'),
-]
--- about/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect, render
-
-
-def showAboutMe(request):
-    return render(request, 'about-me.html')
-
-def showAboutProject(request):
-    return render(request, 'about-project.html')
